b67493ec76a6fef207489a1fc1a0cfc408f237c3
Rajat Singh <heylegend14@gmail.com>
2025-01-18 00:40:18 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-import { useState } from 'react';
+
 import { FaTrashAlt } from "react-icons/fa";
 
 
-function App() {
-  const [groceries, setGroceries] = useState([
-    { id: 1, des: 'Milk', isBought: false },
-    { id: 2, des: 'Bread', isBought: false },
-    { id: 3, des: 'Eggs', isBought: false },
-    { id: 4, des: 'Rice', isBought: false },
-    { id: 5, des: 'Bananas', isBought: true },
-    { id: 6, des: 'Tomatoes', isBought: false },
-    { id: 7, des: 'Potatoes', isBought: false },
-    { id: 8, des: 'Cheese', isBought: false },
-    { id: 9, des: 'Butter', isBought: false },
-    { id: 10, des: 'Onions', isBought: true },
-  ]);
-
+function App(props) {
+  const groceries = props.groceries;
+  const setGroceries = props.setGroceries;
   const handleCheck = (id) => {
     console.log('handleCheck', id);
     const grocerylist = groceries.map((grocery) => {
@@ -81,10 +70,10 @@ function App() {
     </div>
   );
 
-
-
-
 }
+
+
+
 
 
 // import { useState } from "react";
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,34 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx';
 import Header from './header.jsx';
 import Footer from './footer.jsx';
+import { useState } from 'react';
+
+function Main() {
+
+  const [groceries, setGroceries] = useState([
+    { id: 1, des: 'Milk', isBought: false },
+    { id: 2, des: 'Bread', isBought: false },
+    { id: 3, des: 'Eggs', isBought: false },
+    { id: 4, des: 'Rice', isBought: false },
+    { id: 5, des: 'Bananas', isBought: true },
+    { id: 6, des: 'Tomatoes', isBought: false },
+    { id: 7, des: 'Potatoes', isBought: false },
+    { id: 8, des: 'Cheese', isBought: false },
+    { id: 9, des: 'Butter', isBought: false },
+    { id: 10, des: 'Onions', isBought: true },
+  ]);
+
+  return (
+    <div className='mainContent'>
+      <Header heading="Grocery list" />
+      <App groceries={groceries} setGroceries={setGroceries} />
+      <Footer />
+    </div>
+  );
+}
+
 createRoot(document.getElementById('root')).render(
 
 
-  <div className='mainContent'>
-    <Header heading="Grocery list" />
-    <App />
-    <Footer />
-  </div>
-
+  <Main />
 )
